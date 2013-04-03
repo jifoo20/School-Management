d6688342b58da1e583fc1e4211c309ea8b879e7e
jifoo20 <jaafri.ing@gmail.com>
2013-04-03 16:18:45 +0000

    Update required.js

--- School-Management Project/scripts/required.js
+++ School-Management Project/scripts/required.js
@@ -1,1 +1,17 @@
-﻿/**zzzzzzzzzzzzzzzzzzzzzz* @author admin*/var __myNameSpace 	= __myNameSpace || {};(function(){	var	baseFolder		= 'scripts/',	extension		= '.js',	filesToInclude	= ['utils' , 'errors'];		for(var i = 0 , file ; file = filesToInclude[i] ; i++){		include(baseFolder + file + extension);	}})();
+﻿/**
+
+* @author admin nothings
+
+*/
+var __myNameSpace 	= __myNameSpace || {};
+
+(function(){
+	var
+	baseFolder		= 'scripts/',
+	extension		= '.js',
+	filesToInclude	= ['utils' , 'errors'];
+	
+	for(var i = 0 , file ; file = filesToInclude[i] ; i++){
+		include(baseFolder + file + extension);
+	}
+})();
